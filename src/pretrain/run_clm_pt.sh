5920db5f1f3a93534ecc128b638cb150dd0de38c
NLP Journey <31317050+jialinzhang@users.noreply.github.com>
2023-08-27 21:33:53 +0800

    Jialin (#5)
    * 调整.gitignorement删除.vscode
    
    * update .gitignore
    
    * torch.distributed.launch -> torchrun
    
    * log增加rank信息
    
    * 调整ddp位置
    
    * collate_fn
    
    * loss require_grad
    
    * loss = loss / accumulation_steps
    
    * rewrite run_clm_pt.py

--- src/pretrain/run_clm_pt.sh
+++ src/pretrain/run_clm_pt.sh
@@ -1,3 +1,10 @@
+# WandbArguments
+team_name=nlp-journey
+project_name=llama2
+experiment_name=only_train_embed_token_and_lm_head
+experiment_group=llama2-7b训练
+commit_message=llama2-7b训练
+job_type=training
 # DataArguments
 dataset_dir=shibing624/medical
 dataset_subset_name=pretrain
@@ -23,13 +30,23 @@ warmup_ratio=0.05
 output_dir=./output_dir
 do_train=True
 do_eval=True
-eval_steps=1000
+logging_steps=1
+eval_steps=1
+save_steps=10
+save_interval_epoch=1
 per_device_train_batch_size=1
 per_device_eval_batch_size=1
 gradient_accumulation_steps=8
+resume_from_checkpoint=./output_dir
 
 
 CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --nnodes 1 --node_rank 0 run_clm_pt.py \
+                               --team_name ${team_name} \
+                               --project_name ${project_name} \
+                               --experiment_name ${experiment_name} \
+                               --experiment_group ${experiment_group} \
+                               --commit_message ${commit_message} \
+                               --job_type ${job_type} \
                                --dataset_dir ${dataset_dir} \
                                --dataset_subset_name ${dataset_subset_name} \
                                --block_size ${block_size} \
@@ -53,6 +70,10 @@ CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --nnodes 1 --node_rank 0 ru
                                --do_train ${do_train} \
                                --do_eval ${do_eval} \
                                --eval_steps ${eval_steps} \
+                               --save_steps ${save_steps} \
+                               --save_interval_epoch ${save_interval_epoch} \
+                               --logging_steps ${logging_steps} \
                                --per_device_train_batch_size ${per_device_train_batch_size} \
                                --per_device_eval_batch_size ${per_device_eval_batch_size} \
-                               --gradient_accumulation_steps ${gradient_accumulation_steps}
+                               --gradient_accumulation_steps ${gradient_accumulation_steps} \
+                               --resume_from_checkpoint ${resume_from_checkpoint}
