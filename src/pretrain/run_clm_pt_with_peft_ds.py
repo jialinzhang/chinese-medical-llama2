56fb5f2962c1b4e4f336741d2b5106b2f38160b9
NLP Journey <31317050+jialinzhang@users.noreply.github.com>
2023-08-31 20:18:43 +0800

    Jialin (#11)
    * 调整.gitignorement删除.vscode
    
    * update .gitignore
    
    * torch.distributed.launch -> torchrun
    
    * log增加rank信息
    
    * 调整ddp位置
    
    * collate_fn
    
    * loss require_grad
    
    * loss = loss / accumulation_steps
    
    * rewrite run_clm_pt.py
    
    * 修复ddp_setup位置
    
    * 修复load checkpoint内存不足
    
    * add run_clm_pt_with_peft.py
    
    * 修复amp和float16冲突
    
    * 代码格式化
    
    * add run_clm_pt_with_peft_ds.py

--- src/pretrain/run_clm_pt_with_peft_ds.py
+++ src/pretrain/run_clm_pt_with_peft_ds.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-# @File: run_clm_pt_with_peft.py
+# @File: run_clm_pt_with_peft_ds.py
 # @CreateTime: 2023/08/25 14:38:22
 # @WeChat: damo894127201
 # @WeChat Official Accounts: NLP Journey
@@ -13,7 +13,6 @@
         1.1 数据相关参数
         1.2 模型相关参数
         1.3 训练相关参数
-        1.4 实验记录相关参数: wandb
     2、辅助函数
         2.1 指标计算函数
         2.2 tokenizer加载函数
@@ -26,6 +25,8 @@
     1、数据并行DDP
     2、混合精度AMP
     3、Lora微调
+    4、deepspeed
+四、模型监控: deepspeed集成的wandb
 '''
 import numpy as np
 import datasets
@@ -34,7 +35,6 @@ import torch
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.distributed import init_process_group, destroy_process_group
 import transformers
 from transformers import (
     TrainingArguments,
@@ -43,12 +43,10 @@ from transformers import (
     LlamaTokenizer,
     LlamaForCausalLM,
 )
-from transformers.optimization import get_scheduler
-from torch.cuda.amp import autocast, GradScaler
 from peft import get_peft_model, get_peft_model_state_dict, LoraConfig, TaskType
 from peft.peft_model import PeftModelForCausalLM, PeftModel
+import deepspeed
 from sklearn.metrics import accuracy_score
-import wandb
 
 import sys
 import os
@@ -165,7 +163,6 @@ class ModelArguments:
             )
         },
     )
-
     torch_dtype: Optional[str] = field(
         default=None,
         metadata={
@@ -179,7 +176,6 @@ class MyTrainingArguments(TrainingArguments):
     """ 
     与模型训练有关的参数
     
-    TrainingArguments预定义的参数:
     1、learning_rate: 学习率
     2、weight_decay: 权重衰减
     3、adam_beta1: AdamW参数
@@ -211,54 +207,18 @@ class MyTrainingArguments(TrainingArguments):
     与微调模型相关的参数
     
     """
-    
     debug_mode: Optional[bool] = field(default=False, metadata={"help": ("是否调试模式")})
     save_interval_epoch: Optional[int] = field(default=1, metadata={"help": "每多少个epoch保存checkpoint"})
     steps_update: Optional[int] = field(default=0, metadata={"help": "参数已更新次数, 用于加载checkpoint中的step"})
     epochs_run: Optional[int] = field(default=0, metadata={"help": "已训练过的epoch次数, 用于加载checkpoint中的epoch"})
-    use_amp: Optional[bool] = field(default=False, metadata={"help": "是否启用混合精度"})
     # Lora
     lora_rank: Optional[int] = field(default=8, metadata={"help": "Lora矩阵秩"})
     lora_alpha: Optional[float] = field(default=32., metadata={"help": " The alpha parameter for Lora scaling, real value = lora_alpha / r"})
     lora_dropout: Optional[float] = field(default=0.1, metadata={"help": "The dropout probability for Lora layers"})
     lora_trainable: Optional[str] = field(default="q_proj,v_proj", metadata={"help": "The names of the modules to apply Lora to"})
-    modules_to_save : Optional[str] = field(default="embed_tokens,lm_head", metadata={"help": "modules apart from LoRA layers to be set as trainable and saved in the final checkpoint"})
-    
-@dataclass
-class WandbArguments:
-    project_name: Optional[str] = field(default="llama2", metadata={"help": "wandb实验项目名"})
-    team_name: Optional[str] = field(default="nlp-journey", metadata={"help": "wandb团队名"})
-    commit_message: Optional[str] = field(
-        default=None, 
-        metadata={
-            "help": (
-                "A longer description of the run, like a `-m` commit message in git." 
-                "This helps you remember what you were doing when you ran this run."
-            )
-        }
-    )
-    experiment_name: Optional[str] = field(default=None, metadata={"help": "实验名称"})
-    experiment_group: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": (
-                "Specify a group to organize individual runs into a larger experiment."
-                "For example, you might be doing cross validation, or you might have multiple jobs that train and evaluate a model against different test sets."
-                "Group gives you a way to organize runs together into a larger whole, and you can toggle this on and off in the UI"
-                "用于归类实验，比如训练的实验、评估的实验、测试的实验"
-            )
-        }
-    )
-    job_type: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": (
-                "Specify the type of run, which is useful when you're grouping runs together into larger experiments using group."
-                "For example, you might have multiple jobs in a group, with job types like train and eval." 
-                "Setting this makes it easy to filter and group similar runs together in the UI so you can compare apples to apples."
-            )
-        }
-    )
+    modules_to_save: Optional[str] = field(default="embed_tokens,lm_head", metadata={"help": "modules apart from LoRA layers to be set as trainable and saved in the final checkpoint"})
+    # deepspeed
+    deepspeed_config: Optional[str] = field(default=None, metadata={"help": "deepspeed config json path"})
 
 # Step2: 辅助函数
 def metric(y_pred, y_true, normalize=True, sample_weight=None):
@@ -295,9 +255,12 @@ def load_tokenizer(modelArguments: ModelArguments,
     else:
         raise ValueError(f"Local Rank: {trainingArguments.local_rank} 请配置分词器名称或路径: tokenizer_name_or_path")
  
-def ddp_setup():
-    init_process_group(backend="nccl", init_method="env://")
-    torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
+def deepspeed_setup():
+    deepspeed.init_distributed(dist_backend="nccl", init_method="env://")
+
+def read_json(path: str):
+    with open(path, "r", encoding="utf-8") as fi:
+        return json.load(fi)
 
 # Step3: 数据预处理
 def preprocess_dataset(dataArguments: DataArguments,
@@ -390,6 +353,7 @@ def prepare_dataloader(dataArguments: DataArguments,
                        trainingArguments: MyTrainingArguments,
                        logger: logging.RootLogger) -> Tuple[DataLoader]:
     processed_datasets = load_from_disk(dataArguments.processed_data_cache_dir)
+    
     if trainingArguments.do_train:
         train_dataset = processed_datasets['train']
     if trainingArguments.do_eval:
@@ -413,15 +377,22 @@ def prepare_dataloader(dataArguments: DataArguments,
         batch_attention_mask = torch.tensor(batch_attention_mask).cuda(trainingArguments.local_rank)
         batch_label = torch.tensor(batch_label).cuda(trainingArguments.local_rank)
         return batch_input_ids, batch_attention_mask, batch_label
+    
+     # 加载deepspeed配置
+    deepspeed_config = read_json(trainingArguments.deepspeed_config)
+    # mini batch size
+    trainingArguments.per_device_train_batch_size = deepspeed_config["train_micro_batch_size_per_gpu"]
+    trainingArguments.per_device_eval_batch_size = deepspeed_config["train_micro_batch_size_per_gpu"]
+    trainingArguments.gradient_accumulation_steps = deepspeed_config["gradient_accumulation_steps"]
         
     train_sampler = DistributedSampler(dataset=train_dataset)
     train_dataloader = DataLoader(dataset=train_dataset, 
-                                  batch_size=trainingArguments.train_batch_size, 
+                                  batch_size=trainingArguments.per_device_train_batch_size, 
                                   sampler=train_sampler,
                                   collate_fn=collate_fn,
                                   drop_last=True)
     eval_dataloader = DataLoader(dataset=eval_dataset, 
-                                 batch_size=trainingArguments.eval_batch_size,
+                                 batch_size=trainingArguments.per_device_eval_batch_size,
                                  collate_fn=collate_fn,
                                  drop_last=False)
     
@@ -447,11 +418,7 @@ def load_model(modelArguments: ModelArguments,
             modelArguments.torch_dtype
             if modelArguments.torch_dtype in ['auto', None]
             else getattr(torch, modelArguments.torch_dtype)
-    )
-
-    # 如果指定精度为float16或bfloat16则不进行混合精度计算
-    if modelArguments.torch_dtype in ['float16', 'bfloat16']:
-        trainingArguments.use_amp = False
+        )
         
     if modelArguments.model_name_or_path:
         model = LlamaForCausalLM.from_pretrained(
@@ -501,22 +468,12 @@ class MyTrainer:
                  eval_dataloader: DataLoader,
                  trainSampler: DistributedSampler,
                  trainingArguments: MyTrainingArguments,
-                 wandbArguments: WandbArguments,
                  compute_metrics: Callable[[np.ndarray, np.ndarray], Dict],
                  logger: logging.RootLogger
                  ):
         # 静态参数
         self.n_gpus = torch.cuda.device_count()
         self.gpu_id = trainingArguments.local_rank
-        self.lr = trainingArguments.learning_rate
-        self.lr_scheduler_type = trainingArguments.lr_scheduler_type
-        self.adam_beta1 = trainingArguments.adam_beta1
-        self.adam_beta2 = trainingArguments.adam_beta2
-        self.adam_epsilon = trainingArguments.adam_epsilon
-        self.weight_decay = trainingArguments.weight_decay
-        # 每多少个batch进行梯度更新
-        self.gradient_accumulation_steps = trainingArguments.gradient_accumulation_steps
-        self.max_grad_norm = trainingArguments.max_grad_norm
         # lora
         self.lora_rank = trainingArguments.lora_rank
         self.lora_alpha = trainingArguments.lora_alpha
@@ -527,12 +484,13 @@ class MyTrainer:
         
         self.epochs_run = trainingArguments.epochs_run # 已训练epoch个数
         self.steps_update = trainingArguments.steps_update # 已更新参数的次数
+        
         self.num_train_epochs = trainingArguments.num_train_epochs
-        self.train_batch_size = trainingArguments.train_batch_size
-        self.eval_batch_size = trainingArguments.eval_batch_size
+        self.gradient_accumulation_steps = trainingArguments.gradient_accumulation_steps
         self.snapshot_path = trainingArguments.resume_from_checkpoint
         self.save_steps = trainingArguments.save_steps
         self.save_interval_epoch = trainingArguments.save_interval_epoch
+        
         # 采用梯度累积时，指代参数更新的次数
         self.eval_steps = trainingArguments.eval_steps
         self.logging_steps = trainingArguments.logging_steps
@@ -543,67 +501,22 @@ class MyTrainer:
         self.compute_metrics = compute_metrics
         # 加载模型
         self.model = model.to(self.gpu_id)
-        # 动态参数
         self.num_training_steps = self._get_num_training_steps(trainingArguments.max_steps) # 参数最大更新次数
-        self.every_epoch_training_steps = len(self.train_dataloader) // self.gradient_accumulation_steps
-        self.warmup_steps = trainingArguments.get_warmup_steps(self.num_training_steps)
+        self.every_epoch_training_steps = len(self.train_dataloader) // trainingArguments.gradient_accumulation_steps
         self.training_params = self._get_training_params()
         self.num_training_params, self.total_num_params = self._get_num_params()
-        self.optimizer, self.scheduler = self._create_optimizer_and_scheduler()
-        # 混合精度训练
-        self.use_amp = trainingArguments.use_amp
-        self.scaler = GradScaler()
         
         # 数据并行
-        self.model = DDP(self.model, device_ids=[self.gpu_id], find_unused_parameters=True)
-        
-        import wandb
-        self.wandb = wandbArguments
-        self.post_init()
-        
-    def post_init(self):
-        training_config = {
-            'model': 'llama2-7b',
-            'learning_rate': self.lr,
-            'lr_sheduler_type': self.lr_scheduler_type,
-            'adam_beta1': self.adam_beta1,
-            'adam_beta2': self.adam_beta2,
-            'adam_epsilon': self.adam_epsilon,
-            'weight_decay': self.weight_decay,
-            'warmup_steps': self.warmup_steps,
-            'num_training_epochs': self.num_train_epochs,
-            'num_training_steps': self.num_training_steps,
-            'lora_rank': self.lora_rank,
-            'lora_alpha': self.lora_alpha,
-            'lora_dropout': self.lora_dropout,
-            'lora_trainable': self.lora_trainable,
-            'modules_to_save': self.modules_to_save,
-            'max_grad_norm': self.max_grad_norm,
-            'gradient_accumulation_steps': self.max_grad_norm,
-            'train_batch_size': self.train_batch_size,
-            'eval_batch_size': self.eval_batch_size,
-            'n_gpus': self.n_gpus,
-            'every_epoch_training_steps': self.every_epoch_training_steps,
-            'num_training_params': self.num_training_params,
-            'total_num_params': self.total_num_params
-        }
-        wandb.init(
-            config=training_config,
-            project=self.wandb.project_name,
-            entity=self.wandb.team_name,
-            name=self.wandb.experiment_name,
-            notes=self.wandb.commit_message,
-            group=self.wandb.experiment_group,
-            job_type=self.wandb.job_type,
-            reinit=True
-        )
-    
+        self.model, _, _, _ = deepspeed.initialize(args=trainingArguments, 
+                                                   model=self.model, 
+                                                   model_parameters=self.training_params)
+
     def train(self):
         if self.gpu_id == 0:
             percentage = round(self.num_training_params / self.total_num_params * 100, 2)
             self.logger.info(f'Training parameters number is {format(self.num_training_params,",d")}, Total parameters number is {format(self.total_num_params, ",d")}, accounted for {percentage}%')
-        torch.cuda.empty_cache()
         self.model.train()
+        torch.cuda.empty_cache()
         for epoch in trange(1, math.ceil(self.num_train_epochs+1), desc='Epoch', disable=False):
             self._run_epoch(epoch)
             if self.gpu_id == 0 and (epoch % self.save_interval_epoch == 0) or (self.steps_update % self.save_steps == 0):
@@ -630,20 +543,14 @@ class MyTrainer:
         self.model.train()
         metric, avg_loss = metric / (step + 1), avg_loss / (step + 1)
         self.logger.info(f'Local Rank: {self.gpu_id} Evaluate Epoch: {epoch}/{self.num_train_epochs} Step: {self.steps_update} Metric: {metric} Eval Loss: {avg_loss}')
-        wandb.log({"eval_loss": avg_loss}, step=self.steps_update)
-        wandb.log({"eval_accuracy": metric}, step=self.steps_update)
     
     def _run_epoch(self, epoch: int):
         self.trainSampler.set_epoch(epoch)
-        self.optimizer.zero_grad()
-        # 在梯度累积步数内的损失
         cur_loss = 0
         for batch_index, batch in enumerate(self.train_dataloader):
             loss = self._run_batch(batch)
             cur_loss += loss
-            # 当经过梯度累积步数个batch后，进行参数更新
             if (batch_index + 1) % self.gradient_accumulation_steps == 0:
-                self._update_params()
                 self.steps_update += 1 # 记录参数更新次数
                 if self.gpu_id == 0:
                     if self.steps_update % self.logging_steps == 0:
@@ -657,41 +564,18 @@ class MyTrainer:
                      
     def _run_batch(self, batch):
         batch_input_ids, batch_attention_mask, batch_label = batch
-        if self.use_amp:
-            with autocast():
-                outputs = self.model(input_ids=batch_input_ids, attention_mask=batch_attention_mask, labels=batch_label)
-        else:
-            outputs = self.model(input_ids=batch_input_ids, attention_mask=batch_attention_mask, labels=batch_label)
+        outputs = self.model(input_ids=batch_input_ids, attention_mask=batch_attention_mask, labels=batch_label)
         loss = outputs[0]
         loss.requires_grad_(True)
-        # 判断是否进行梯度累积，如果进行，则将损失值除以累积步数
-        if self.gradient_accumulation_steps > 0:
-            loss = loss / self.gradient_accumulation_steps
-        # 计算梯度值,并累加梯度
-        if self.use_amp:
-            self.scaler.scale(loss).backward()
-        else:
-            loss.backward()
-        # 裁剪梯度
-        torch.nn.utils.clip_grad_norm_([p for n,p in self.training_params], self.max_grad_norm)
+        # 计算梯度值
+        self.model.backward(loss)
+        # 参数更新/学习率调整/梯度累积
+        self.model.step()
         return loss.item()
-    
-    def _update_params(self):
-        # 执行参数更新
-        if self.use_amp:
-            self.scaler.step(self.optimizer)
-            self.scaler.update()
-        else:
-            self.optimizer.step()
-        # 学习率调整
-        self.scheduler.step()
-        # 梯度清零
-        self.optimizer.zero_grad()
         
     def _log_training_info(self, epoch:int, loss: float):
         content = f'Local Rank: {self.gpu_id} | Epoch {epoch}/{self.num_train_epochs} | Step {self.steps_update}/{self.num_training_steps} | Loss: {loss}'
         self.logger.info(content)
-        wandb.log({'training_loss': loss}, step=self.steps_update)
     
     def _save_snapshot(self, epoch: int):
         peft_model_dir = os.path.join(self.output_dir, f"pt_lora_model_epoch_{epoch}_step_{self.steps_update}")
@@ -703,39 +587,6 @@ class MyTrainer:
             info = {"EPOCHS_RUN": self.epochs_run, "STEP": self.steps_update}
             json.dump(info, fo, indent=4, ensure_ascii=False)
         self.logger.info(f'Local Rank: {self.gpu_id} Epoch {epoch} STEP {self.steps_update}| Training snapshot saved at {peft_model_dir}')
-    
-    def _create_optimizer_and_scheduler(self):
-        optimizer = self._create_optimizer()
-        scheduler = self._create_scheduler(optimizer)
-        return optimizer, scheduler
-    
-    def _create_optimizer(self):
-        no_decay = ['bias', 'layernorm', 'norm']
-        optimizer_grouped_parameters = [
-            {
-                'params': [p for n, p in self.training_params if not any(nd in n for nd in no_decay)],
-                'weight_decay': self.weight_decay
-            },
-            {
-                'params': [p for n, p in self.training_params if any(nd in n for nd in no_decay)],
-                'weight_decay': 0.0
-            }
-        ]
-        optimizer = torch.optim.AdamW(params=optimizer_grouped_parameters,
-                                      lr=self.lr,
-                                      betas=(self.adam_beta1, self.adam_beta2),
-                                      eps=self.adam_epsilon,
-                                      weight_decay=self.weight_decay)
-        return optimizer
-    
-    def _create_scheduler(self, optimizer: torch.optim.Optimizer = None):
-        lr_scheduler = get_scheduler(
-            self.lr_scheduler_type,
-            optimizer=optimizer,
-            num_warmup_steps=self.warmup_steps,
-            num_training_steps=self.num_training_steps,
-        )
-        return lr_scheduler
 
     def _get_num_training_steps(self, max_steps):
         if max_steps > 0:
@@ -765,10 +616,10 @@ class MyTrainer:
 
 def main():
     # 初始化进程组
-    ddp_setup()
+    deepspeed_setup()
     # 参数解析
-    parser = HfArgumentParser((ModelArguments, DataArguments, MyTrainingArguments, WandbArguments))
-    modelArguments, dataArguments, trainingArguments, wandbArguments = parser.parse_args_into_dataclasses()
+    parser = HfArgumentParser((ModelArguments, DataArguments, MyTrainingArguments))
+    modelArguments, dataArguments, trainingArguments = parser.parse_args_into_dataclasses()
     # 日志记录
     logger = logging.getLogger(f'Rank:{trainingArguments.local_rank} {__name__}')
     logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", 
@@ -788,11 +639,9 @@ def main():
                         eval_dataloader=eval_dataloader, 
                         trainSampler=trainSampler, 
                         trainingArguments=trainingArguments,
-                        wandbArguments=wandbArguments,
                         compute_metrics=compute_metrics,
                         logger=logger)
     trainer.train()
-    destroy_process_group()
     
 if __name__ == '__main__':
     main()
---
+++ src/pretrain/run_clm_pt_with_peft_ds.sh
@@ -0,0 +1,56 @@
+# DataArguments
+dataset_dir=shibing624/medical
+dataset_subset_name=pretrain
+block_size=512
+processed_data_cache_dir=./processed
+# ModelArguments
+model_name_or_path=meta-llama/Llama-2-7b-hf
+tokenizer_name_or_path=../../data/chinese-medical-llama-tokenizer/hf_dir/
+use_auth_token=*****
+torch_dtype=float16
+# TrainingArguments
+lora_rank=8
+lora_alpha=32.
+lora_dropout=0.1
+lora_trainable="q_proj,v_proj,k_proj,o_proj,gate_proj,down_proj,up_proj"
+modules_to_save=embed_tokens,lm_head
+debug_mode=True
+deepspeed_config=./ds_config.json
+max_steps=100 # 参数最大更新次数
+num_train_epochs=1
+do_train=True
+do_eval=True
+logging_steps=1
+eval_steps=1
+save_steps=10
+save_interval_epoch=1
+output_dir=./output_dir
+resume_from_checkpoint=./output_dir
+
+
+CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --nnodes 1 --node_rank 0 run_clm_pt_with_peft_ds.py \
+                               --dataset_dir ${dataset_dir} \
+                               --dataset_subset_name ${dataset_subset_name} \
+                               --block_size ${block_size} \
+                               --processed_data_cache_dir ${processed_data_cache_dir} \
+                               --model_name_or_path ${model_name_or_path} \
+                               --tokenizer_name_or_path ${tokenizer_name_or_path} \
+                               --use_auth_token ${use_auth_token} \
+                               --torch_dtype ${torch_dtype} \
+                               --lora_rank ${lora_rank} \
+                               --lora_alpha ${lora_alpha} \
+                               --lora_dropout ${lora_dropout} \
+                               --lora_trainable ${lora_trainable} \
+                               --modules_to_save ${modules_to_save} \
+                               --debug_mode ${debug_mode} \
+                               --deepspeed_config ${deepspeed_config} \
+                               --max_steps ${max_steps} \
+                               --num_train_epochs ${num_train_epochs} \
+                               --do_train ${do_train} \
+                               --do_eval ${do_eval} \
+                               --logging_steps ${logging_steps} \
+                               --eval_steps ${eval_steps} \
+                               --save_steps ${save_steps} \
+                               --save_interval_epoch ${save_interval_epoch} \
+                               --output_dir ${output_dir} \
+                               --resume_from_checkpoint ${resume_from_checkpoint}
